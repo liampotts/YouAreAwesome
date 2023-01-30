257c1f62e6de5c559e478a8395049ace06242d27
liampotts <pottsw@bc.edu>
2023-01-30 10:01:45 -0500

    finished 1.9

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,76 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = ""
     
     var body: some View {
-        ZStack {
+        
+        VStack {
             
-            Rectangle()
-                .fill(
-                    Gradient(colors: [Color("SkyBlue"), .indigo])
-                
-                )
-                .ignoresSafeArea()
-            VStack {
-                
-                Text("You have skills!")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .foregroundColor(Color("Gold-BC"))
-                    .padding()
-                    .background(Color("Maroon-BC"))
-                    .cornerRadius(15)
-                
-                
-                
-                
-                Spacer()
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
-    //                .border(.orange, width: 1)
-                    .padding()
-                //test comment
-                
-                Spacer()
-                
-                HStack{
-                    
-                    Button ("Awesome"){
-                        //action that happens when button is pressed
-                        messageString = "You Are Awesome!"
-                    }
-                    
-                    .buttonStyle(.borderedProminent)
-                    Spacer()
-                        
-                    
-                    Button ("Great"){
-                        //action that happens when button is pressed
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    
-                }
-    //            .border(.purple,width: 5)
+            Spacer()
+            
+            
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            //                .border(.orange, width: 1)
                 .padding()
+            //test comment
+            
+            Spacer()
+            
+        
+            Button ("Show Message"){
                 
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great!"
+                //action that happens when button is pressed
+                if messageString == message1 {
+                    messageString = message2
+                } else{
+                    messageString = message1
+                }
             }
+            .buttonStyle(.borderedProminent)
         }
         
         
-        
-        
     }
-    
+       
 }
+
 
 
 struct ContentView_Previews: PreviewProvider {
