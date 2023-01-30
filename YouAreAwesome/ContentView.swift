dc692d536f971c5ddbf9d43aa41a96b5d182a196
liampotts <pottsw@bc.edu>
2023-01-29 23:19:20 -0500

    finished 1.6

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,38 +11,65 @@ struct ContentView: View {
     @State private var messageString = "When the Genius Bar needs help, they call you!"
     
     var body: some View {
-        VStack {
+        ZStack {
             
-            
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.orange, width: 1)
+            Rectangle()
+                .fill(
+                    Gradient(colors: [Color("SkyBlue"), .indigo])
+                
+                )
+                .ignoresSafeArea()
+            VStack {
+                
+                Text("You have skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Maroon-BC"))
+                    .cornerRadius(15)
+                
+                
+                
+                
+                Spacer()
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 1)
+                    .padding()
+                //test comment
+                
+                Spacer()
+                
+                HStack{
+                    
+                    Button ("Awesome"){
+                        //action that happens when button is pressed
+                        messageString = "You Are Awesome!"
+                    }
+                    
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                        
+                    
+                    Button ("Great"){
+                        //action that happens when button is pressed
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    
+                }
+    //            .border(.purple,width: 5)
                 .padding()
-            //test comment
-            
-            HStack{
-                
-                Button ("Awesome"){
-                    //action that happens when button is pressed
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
-                
-                Button ("Great"){
-                    //action that happens when button is pressed
-                    messageString = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
                 
             }
-            .border(.purple,width: 5)
-            
         }
         
         
